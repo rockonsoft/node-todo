77bedbaaba1523d5faf7c4f673c60d4b13092e49
hanob <hanobotha@gmail.com>
2016-03-15 17:10:00 +0200

    local config

--- config/database.js
+++ config/database.js
@@ -1,4 +1,5 @@
 module.exports = {
-    remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
+    //remoteUrl : 'mongodb://node:nodeuser@mongo.onmodulus.net:27017/uwO3mypu',
+    remoteUrl : 'mongodb://timeadmin:timeadmin@jello.modulusmongo.net:27017/etoT7ide',
     localUrl: 'mongodb://localhost/meanstacktutorials'
 };
--- server.js
+++ server.js
@@ -9,8 +9,8 @@ var bodyParser = require('body-parser');
 var methodOverride = require('method-override');
 
 // configuration ===============================================================
-mongoose.connect(database.localUrl); 	// Connect to local MongoDB instance. A remoteUrl is also available (modulus.io)
-
+// mongoose.connect(database.localUrl); 	// Connect to local MongoDB instance. A remoteUrl is also available (modulus.io)
+mongoose.connect(database.remoteUrl);
 app.use(express.static(__dirname + '/public')); 		// set the static files location /public/img will be /img for users
 app.use(morgan('dev')); // log every request to the console
 app.use(bodyParser.urlencoded({'extended': 'true'})); // parse application/x-www-form-urlencoded
